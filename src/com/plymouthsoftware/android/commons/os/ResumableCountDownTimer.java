9764aa1d412becf992c26a07da21df70439af4d4
Chris Blunt <chris@plymouthsoftware.com>
2012-09-02 20:12:08 +0100

    Added ResumableCountdownTimer

---
+++ src/com/plymouthsoftware/android/commons/os/ResumableCountDownTimer.java
@@ -0,0 +1,103 @@
+
+package com.plymouthsoftware.android.commons.os;
+
+import android.os.CountDownTimer;
+
+public abstract class ResumableCountDownTimer {
+
+    private long startTime;
+    private long splitTime;
+    private long millisRemaining;
+    private long elapsed;
+    private long splitElapsed;
+
+    private boolean inProgress;
+    private boolean isPaused;
+
+    private CountDownTimer countDownTimer;
+
+    public ResumableCountDownTimer() {
+        inProgress = false;
+        isPaused = false;
+    }
+
+    public void start(long millis, boolean restart) {
+        if (inProgress && !isPaused)
+            return;
+
+        splitElapsed = 0;
+
+        if (restart) {
+            startTime = System.currentTimeMillis();
+            splitTime = startTime;
+            elapsed = 0;
+        }
+        else
+            splitTime = System.currentTimeMillis();
+
+        countDownTimer = new CountDownTimer(millis, 250) { // Update every second
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                millisRemaining = millisUntilFinished;
+
+                long now = System.currentTimeMillis();
+                splitElapsed = now - splitTime;
+
+                ResumableCountDownTimer.this.onTick(elapsed + splitElapsed, millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                ResumableCountDownTimer.this.onFinish();
+                inProgress = false;
+            }
+        };
+
+        countDownTimer.start();
+        inProgress = true;
+    }
+
+    public void pause() {
+        if (!inProgress || countDownTimer == null)
+            return;
+
+        countDownTimer.cancel();
+        countDownTimer = null;
+
+        elapsed += splitElapsed;
+
+        isPaused = true;
+    }
+
+    public void resume() {
+
+        if (millisRemaining <= 1) {
+            isPaused = false;
+            return;
+        }
+
+        splitTime = System.currentTimeMillis();
+        splitElapsed = 0;
+
+        start(millisRemaining, false);
+
+        isPaused = false;
+
+    }
+
+    public abstract void onTick(long millisElapsed, long millisRemaining);
+
+    public abstract void onFinish();
+
+    public void toggle() {
+        if (isPaused)
+            resume();
+        else
+            pause();
+    }
+
+    public boolean inProgress() {
+        return inProgress;
+    }
+}
